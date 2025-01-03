27cb95b5215b20d77ef6edccbfca62bb99c0d40d
makramab <m.akram.bari@nyu.edu>
2025-01-04 05:13:28 +0700

    feat: init project

---
+++ src/main/java/id/agrowculture/sika/SikaApplication.java
@@ -0,0 +1,13 @@
+package id.agrowculture.sika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/agrowculture/sika/SikaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.agrowculture.sika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
